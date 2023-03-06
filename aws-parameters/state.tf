c2c959648491de6bc05d05b9da375066aa98a6b8
Prabhu <prabh.rao@gmail.com>
2023-03-06 19:29:28 +0530

    roboshop-ansible common

--- aws-parameters/state.tf
+++ aws-parameters/state.tf
@@ -4,12 +4,10 @@ terraform {
 
 resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
-#  name  = var.parameters[count.index].name
-#  type  = var.parameters[count.index].type
-#  value = var.parameters[count.index].value
-   name  = "foo"
-   type  = "string"
-   value = "bar"
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+
 }
 
 variable "parameters" {}
