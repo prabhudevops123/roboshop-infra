de3a972e109d9287b9d755455b0ed9de74d50c74
Prabhu <prabh.rao@gmail.com>
2023-03-07 22:11:43 +0530

    roboshop-ansible parameters secrets

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,7 +18,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/raghudevopsb71/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${var.env}"
+      "ansible-pull -i localhost, -U https://github.com/prabhudevops123/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${var.env}"
     ]
 
   }
@@ -51,7 +51,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z103214126L48SQW30RSR"
-  name    = "${var.component}-dev.devopsb71.online"
+  name    = "${var.component}-dev.devopsprabhu.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
